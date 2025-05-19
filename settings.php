7583f0998a3183db15e36d67464af887c039d938
CHRISTIAN ROASA <109757954+ashintaru@users.noreply.github.com>
2025-05-19 11:30:39 +0800

    sample

--- settings.php
+++ settings.php
@@ -69,7 +69,8 @@ $sy = ''.$py.' - '.$year.'';
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
- 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
  
  
 <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
